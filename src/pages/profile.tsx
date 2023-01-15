ec66772e31d71d73501862aa0a19df67fa37fbd0
mczolko-lego <michael.czolko@consultant.LEGO.com>
2023-01-16 00:37:05 +0100

    typesafety with react query

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,42 +1,8 @@
-import { useEffect, useState } from 'react'
-import { Data } from '@/pages/api/profile'
+import ProfileComponent from '@/components/Profile'
 
 
-async function getProfile(): Promise<Data> {
-  const profileResponse = await fetch('/api/profile')
-  if (!profileResponse.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return profileResponse.json()
-}
-
-
-export default function Profile({ initProfile }: { initProfile?: Data }) {
-  const [ profile, setProfile ] = useState<Data>(initProfile)
-
-  useEffect(() => {
-    getProfile().then(setProfile)
-  }, [])
-
-  if (!profile) {
-    return <div>Loading...</div>
-  }
-
+export default function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
-      <div>{profile.name}</div>
-      <div>{profile.bio}</div>
-    </div>
+    <ProfileComponent />
   )
 }
-
-
-export const getServerSideProps = async () => ({
-  props: {
-    initProfile: {
-      name: 'Nemam',
-      bio: 'Lorem ipsum dolor sit amet'
-    }
-  }
-})
